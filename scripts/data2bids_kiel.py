40a0ff782ab303a12644329ed189b5883eb502ab
jwelzel <52565341+JuliusWelzel@users.noreply.github.com>
2025-07-02 16:46:38 +0200

    Add new packages and update data processing in data2bids_kiel.py
    - Added new dependencies in poetry.lock: defusedxml, joblib, llvmlite, numba, pybv, scikit-learn, threadpoolctl, tslearn.
    - Enhanced data2bids_kiel.py to include motion capture data processing.
    - Implemented marker data extraction and filtering for markers with less than 30 frames.
    - Added plotting functionality for marker events and positions.
    - Updated BIDS path handling for motion capture data.
    - Integrated K-Means clustering for time series data using tslearn.

--- scripts/data2bids_kiel.py
+++ scripts/data2bids_kiel.py
@@ -1,4 +1,5 @@
 import json
+import matplotlib.pyplot as plt
 from mne_bids import make_dataset_description, write_raw_bids, BIDSPath
 from mnelab.io.xdf import read_raw_xdf
 import mne
@@ -8,7 +9,21 @@ import pyxdf
 
 from utils.motionbids import generate_channels_tsv, generate_motion_json_file
 from utils.config import DIR_BIDS_ROOT, DIR_PROJ
+from utils.plotting import plot_marker_events
 
+##################################
+# Create a dataset description
+##################################
+# This is a required step for BIDS datasets
+# It creates a dataset_description.json file in the BIDS root directory
+make_dataset_description(path=DIR_BIDS_ROOT, name='StepUp_Sydney')
+
+
+
+
+###################################
+# EEG 2 BIDS
+###################################
 # load data
 file_path = Path(r"C:\Users\juliu\Desktop\kiel\stepup_bids_converter\data\source")  # Replace with your XDF file path)
 
@@ -54,8 +69,8 @@ raw.set_montage(montage)
 raw.info['line_freq'] = 50  # specify power line frequency as required by BIDS
 
 # write raw data to BIDS
-bids_path = BIDSPath(subject=subject_id, task=task, session=session, datatype='eeg', root=DIR_BIDS_ROOT)
-write_raw_bids(raw, bids_path, overwrite=True, allow_preload=True, format='BrainVision', verbose=True)
+bids_path_eeg = BIDSPath(subject=subject_id, task=task, session=session, datatype='eeg', root=DIR_BIDS_ROOT)
+write_raw_bids(raw, bids_path_eeg, overwrite=True, allow_preload=True, format='BrainVision', verbose=True)
 
 print(f'Finished writing EEG BIDS for participant {subject_id} and task {task}')
 
@@ -69,48 +84,90 @@ print(f'Finished writing EEG BIDS for participant {subject_id} and task {task}')
 
 
 
-
+####################################
+# Mocap 2 BIDS
+####################################
+# create bids path for motion capture data
+bids_path_motion = BIDSPath(subject=subject_id, task=task, session=session, datatype='motion', root=DIR_BIDS_ROOT)
 
 # find Mocap stream
-# load EEG stream into MNE and export to BIDS
-data_stream = [s for s in streams if s['info']['type'][0] == '6D'][0]
-data_stream_id = data_stream['info']['stream_id']
+mocap_stream = [s for s in streams if s['info']['type'][0] == '6D'][0]
 
-data_lsl_times = data_stream['time_stamps'] - data_stream['time_stamps'][0]
-data_raw = data_stream['time_series']
+mocap_times = mocap_stream['time_stamps'] - mocap_stream['time_stamps'][0]
+mocap_raw = mocap_stream['time_series']
 
-# extract mocap marker data
-mocap_times = data_raw[:,0]
-frames_mocap = data_raw[:,1]
-vicon_data = data_raw[:,3:83]
-emg_data = data_raw[:,83:723]
+
+# print unique marker ids, and number of occurences per makrer id
+marker_ids = mocap_raw[:,3]
+unique_marker_ids = set(marker_ids)
+for marker_id in unique_marker_ids:
+    print(f"Marker {int(marker_id)}: {np.sum(marker_ids == marker_id)}")
+    
+# remove all markers which have less than 30 frames
+for marker_id in unique_marker_ids:
+    if np.sum(marker_ids == marker_id) < 30:
+        idx = np.where(marker_ids == marker_id)
+        mocap_raw = np.delete(mocap_raw, idx, axis=0)
+        marker_ids = np.delete(marker_ids, idx)
+
+     
+# Count occurrences of each marker ID
+marker_ids = mocap_raw[:, 3]
+unique_marker_ids, counts = np.unique(marker_ids, return_counts=True)
+
+# Get the indices and values of most common markers
+most_common_markers = sorted(zip(unique_marker_ids, counts), key=lambda x: x[1], reverse=True)
+
+# Store the data and indices for the 5 most common markers in a dictionary
+marker_data_dict = {}
+for marker_id, count in most_common_markers:
+    indices = np.where(marker_ids == marker_id)
+    marker_data = mocap_raw[indices]
+    marker_data_dict[int(marker_id)] = {
+        "data": marker_data,
+        "indices": indices
+    }
+
+
+# Print the dictionary for verification
+for marker_id, info in marker_data_dict.items():
+    indices = info['indices']
+    print(f"Marker {marker_id}:")
+    if len(indices) > 0:
+        print(f"  First index: {indices[0][0]}, Last index: {indices[0][-1]}")
+    else:
+        print("  No indices found.")
+
+
+# plot marker events
+fig = plot_marker_events(marker_data_dict, show=False)
+
+
+# plot ssecond against time for each unique marker id
+dict_raw = {}
+fig, ax = plt.subplots(3, 1)
+for marker_id in unique_marker_ids:
+    idx = np.where(marker_ids == marker_id)
+    ax[0].plot(mocap_times[idx], mocap_raw[idx, 0].T)
+    ax[1].plot(mocap_times[idx], mocap_raw[idx, 1].T)
+    ax[2].plot(mocap_times[idx], mocap_raw[idx, 2].T)
+    dict_raw[str(int(marker_id))] = mocap_raw[idx,:].T
+#ax.set_xlim([0, 10])
+#ax.set_ylim([393.8, 394.2])
 
 
 # create bids dataset
-TRACKSYS = 'Vicon'
-SRATE_MOCAP = float(data_stream['info']['nominal_srate'][0])  # Sampling frequency for motion capture data
+TRACKSYS = 'Qualisys'  # Track system used for motion capture
+SRATE_MOCAP = float(mocap_stream['info']['nominal_srate'][0])  # Sampling frequency for motion capture data
 
-# Create a dataset description
-make_dataset_description(path=DIR_BIDS_ROOT, name='StepUp_Sydney')
 
-bids_path = BIDSPath(subject=subject_id, task=task, session=session, datatype='motion', root=DIR_BIDS_ROOT)
+# plot marker data
+fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+for marker_id, info in marker_data_dict.items():
+    ax.plot(info['data'][:, 0], info['data'][:, 2], label=f'Marker {marker_id}')
+ax.set_xlabel('X Position')
+ax.set_ylabel('Y Position')
 
-# write channels.tsv to path
-channels = generate_channels_tsv(["Pelvis", "LeftFoot", "RightFoot"])
-channels_tsv = bids_path.copy().update(suffix='channels', extension='.tsv')
-channels_tsv = str(channels_tsv).split('_channels')
-channels_tsv = channels_tsv[0] + '_tracksys-' + TRACKSYS + '_channels' + channels_tsv[1]
-channels.to_csv(channels_tsv, sep='\t', index=False)
-
-# write motion.json to path
-motion_fields = dict(TaskName=task, SamplingFrequency=SRATE_MOCAP)
-motion_json = generate_motion_json_file(motion_fields)
-motion_json_file = bids_path.copy().update(suffix='motion', extension='.json')
-motion_json_file = str(motion_json_file).split('_motion')
-motion_json_file = motion_json_file[0] + '_tracksys-' + TRACKSYS + '_motion' + motion_json_file[1]
-with open(Path(motion_json_file), 'w') as f:
-    json.dump(motion_json, f)
-    
 # write raw data as tsv
 # make vstack in order pelvis, left foot, right foot
 pelvis = vicon_data
@@ -122,10 +179,39 @@ raw_data = np.vstack([pelvis, left_foot, right_foot]).T
 raw_data = np.squeeze(raw_data)
 
 #store as tsv without headers
-raw_data_tsv = str(bids_path.copy().update(suffix='motion', extension='.tsv'))
+raw_data_tsv = str(bids_path_motion.copy().update(suffix='motion', extension='.tsv'))
 
 # split string before _motion and insert tracksys-Qualisys
 raw_data_tsv = raw_data_tsv.split('_motion')
 raw_data_tsv = raw_data_tsv[0] + '_tracksys-' + TRACKSYS + '_motion' + raw_data_tsv[1]
 
 np.savetxt(raw_data_tsv, raw_data, delimiter='\t', header='', comments='')
+
+# write channels.tsv to path
+channels = generate_channels_tsv(["Pelvis", "LeftFoot", "RightFoot"])
+channels_tsv = bids_path_motion.copy().update(suffix='channels', extension='.tsv')
+channels_tsv = str(channels_tsv).split('_channels')
+channels_tsv = channels_tsv[0] + '_tracksys-' + TRACKSYS + '_channels' + channels_tsv[1]
+channels.to_csv(channels_tsv, sep='\t', index=False)
+
+# write motion.json to path
+motion_fields = dict(TaskName=task, SamplingFrequency=SRATE_MOCAP)
+motion_json = generate_motion_json_file(motion_fields)
+motion_json_file = bids_path_motion.copy().update(suffix='motion', extension='.json')
+motion_json_file = str(motion_json_file).split('_motion')
+motion_json_file = motion_json_file[0] + '_tracksys-' + TRACKSYS + '_motion' + motion_json_file[1]
+with open(Path(motion_json_file), 'w') as f:
+    json.dump(motion_json, f)
+
+
+
+from tslearn.generators import random_walks
+X = random_walks(n_ts=50, sz=32, d=1)
+
+# Apply K-Means clustering using DTW as the distance measure
+model = TimeSeriesKMeans(n_clusters=3, metric="euclidean", max_iter=1)
+clusters = model.fit_predict(aligned_series)
+
+
+# Output the cluster assignments
+print(clusters)
